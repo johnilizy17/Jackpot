54ad2e4d1269741c41e7434126a1fbdfc2b9f7b5
johnilizy17 <johnabrahamtosin@gmail.com>
2023-09-29 14:00:41 +0100

    adding it to footer

--- src/components/Dashboard/Presale.js
+++ src/components/Dashboard/Presale.js
@@ -1,14 +1,14 @@
+import Link from 'next/link';
 import { Box, Center, Flex, IconButton } from '@chakra-ui/react';
 import React from 'react';
 import { BsDiscord, BsFacebook, BsTelegram, BsTwitter } from 'react-icons/bs';
 import { IoLogoTwitter } from 'react-icons/io';
-import Link from 'next/link';
 
-export default function Presale() {
+export default function Footer() {
 
     return (
-        <Box mt="30px" color="#fff" m="20px" mt="0px" p="20px" borderRadius="16px" background="#1f1c4a" >
-            <Box mt='20px'>
+        <div className="footer_container">
+            <Box pos="absolute" top="0px" p="20px" zIndex="1000" color="#fff" w="100vw">
                 <Box fontSize="17px" fontWeight="800">
                     <Box mb="20px">JACKPOT INFO</Box>
                 </Box>
@@ -17,15 +17,25 @@ export default function Presale() {
                 <Box> Duration: 2.5 mins(Max. Buy 20$) </Box>
                 <Box mt="20px">
                     <Box>REWARD DISTRIBUTION</Box>
-                    <Box>50% WINNER</Box>
-                    <Box>
-                        20% NEXT NORMAL JACKPOT
-                    </Box>
-                    <Box>20% BIG JACKPOT</Box>
-                    <Box>5% MARKETING</Box>
-                    <Box>5% BOMB PRIZ</Box>
+                    <Flex flexWrap="wrap">
+                        <Box mr="10px" mt="10px">50% WINNER</Box>
+                        <Box mr="10px" mt="10px">
+                            20% NEXT NORMAL JACKPOT
+                        </Box>
+                        <Box mr="10px" mt="10px">20% BIG JACKPOT</Box>
+                        <Box mr="10px" mt="10px">5% MARKETING</Box>
+                        <Box mr="10px" mt="10px">5% BOMB PRIZ</Box>
+                    </Flex>
                 </Box>
-                <Center flexDir="column" mt="40px">
+            </Box>
+            <footer class="footer">
+                <div class="waves">
+                    <div class="wave" id="wave1"></div>
+                    <div class="wave" id="wave2"></div>
+                    <div class="wave" id="wave3"></div>
+                    <div class="wave" id="wave4"></div>
+                </div>
+                <Center flexDir="column" mt="40px" color="#fff">
                     <Box fontWeight="800" fontSize="24px">FIND US ON SOCIAL</Box>
                     <Flex mt="10px">
                         <IconButton background="transparent" mr="5px">
@@ -41,22 +51,26 @@ export default function Presale() {
                             <BsFacebook />
                         </IconButton>
                     </Flex>
-                    <Box mt="30px">
-                        Features
-                    </Box>
-                    <Box mt="10px">
-                        How it works
-                    </Box>
-                    <Box mt="10px">
-                        Token info
-                    </Box>
-                    <Link href="/about">
-                        <Box mt="10px">
-                            About us
+                    <Center flexWrap="wrap">
+                        
+                        <Box mt="10px" mr="10px">
+                            Features
                         </Box>
-                    </Link>
+                        <Box mt="10px" mr="10px">
+                            How it works
+                        </Box>
+                        <Box mt="10px" mr="10px">
+                            Token info
+                        </Box>
+                        <Link href="/about">
+                            <Box mt="10px" mr="10px">
+                                About us
+                            </Box>
+                        </Link>
+                    </Center>
                 </Center>
-            </Box >
-        </Box >
+                <p>&copy;2021 ALPHA | All Rights Reserved</p>
+            </footer>
+        </div>
     )
 }
