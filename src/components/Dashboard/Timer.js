ab8b26c16d22166f12e0819bbd5cd7a047e1dcb1
johnilizy17 <johnabrahamtosin@gmail.com>
2023-10-10 06:23:14 +0100

    the new account

--- src/components/Dashboard/Timer.js
+++ src/components/Dashboard/Timer.js
@@ -35,9 +35,9 @@ export default function TimeCounter({ date, setDate }) {
 
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var hours = Math.floor(24-(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor(60-(distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor(60-(distance % (1000 * 60)) / 1000);
 
         setNumberOfTime({ hour: hours, min: minutes, sec: seconds })
     }
