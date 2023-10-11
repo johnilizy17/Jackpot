0b0f5ab8a2f52409f5685969c5df357519fbca5c
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-11 18:25:55 +0100

    Time testing

--- src/components/Dashboard/Timer.js
+++ src/components/Dashboard/Timer.js
@@ -38,7 +38,7 @@ const [DownDate2, setDownDate2] = useState();
         let now =  new Date().getTime()
      
         var distance =   DownDate - now ;
-alert(distance, DownDate) 
+alert( DownDate) 
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
