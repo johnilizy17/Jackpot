253d1d2876d3a4ff261ad39e76284f3279b7876a
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-11 16:34:04 +0100

    Update NFT.js address

--- src/services/NFT.js
+++ src/services/NFT.js
@@ -1,2 +1,2 @@
-export const contractAddress = "0x1198655Dd92A1e6BCb44f47b2D017D9d6f7DFf78"
+export const contractAddress = "0xe679b1160304B9B204a8D3380e9f6337a9ae4314"
 export const BUSD = "0xaB1a4d4f1D656d2450692D237fdD6C7f9146e814"
