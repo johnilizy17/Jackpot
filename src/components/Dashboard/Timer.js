3a8df3b13258a6e0dc6122ee89e2859bb7844f46
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-11 19:56:21 +0100

    Timer

--- src/components/Dashboard/Timer.js
+++ src/components/Dashboard/Timer.js
@@ -40,10 +40,9 @@ const [DownDate2, setDownDate2] = useState();
         var distance =  DownDate - Math.floor(now/1000) ;
         alert(distance)
         // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var hours = Math.floor((distance % ( 60 * 60 * 24)) / ( 60 * 60));
+  var minutes = Math.floor((distance % (60 * 60)) / (60));
+  var seconds = Math.floor((distance % (60)) );
         
         setNumberOfTime({ hour: hours, min: minutes, sec: seconds })
    if(distance <0){ 
