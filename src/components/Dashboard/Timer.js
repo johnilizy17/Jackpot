c5dadb7dc30f025b35d9a7f8b1c933aecbec60a5
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-11 19:37:10 +0100

    Finance da

--- src/components/Dashboard/Timer.js
+++ src/components/Dashboard/Timer.js
@@ -36,10 +36,10 @@ const [DownDate2, setDownDate2] = useState();
 
         // Find the distance between now and the count down date
         let now =  new Date().getTime()
-        alert(now)
-        console.log(now)
+        
         var distance =  DownDate - Math.floor(now/1000) ;
-    
+    alert(distance)
+        
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
