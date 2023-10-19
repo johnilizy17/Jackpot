dc48ffe1d9520132aabe01e107fe828be1922350
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-19 16:12:25 +0100

    Price time

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -312,7 +312,7 @@ export default function DashboardDesktop() {
                         <Box className="minor-bar">
                             <Box className="labels">
                                 <p>{type === 1 ? "Big Bang" : "Normal"}</p>
-                                <p>${jackpotData[9] ? JSON.parse(jackpotData[0]).toFixed(2) : 0}/$10k</p>
+                                <p>${type === 1 ? JSON.parse(bigBangPrice).toFixed(2) : JSON.parse(bigBangPrice).toFixed(2) }/$10k</p>
                             </Box>
                             <Box className="progress-bar ">
                                 <Box className="bar" style={percentage > 100 ? { width: `100%` } : type === 1 ? { width: `${percentage}%` } : { width: `${100 - percentage}%` }}></Box>
