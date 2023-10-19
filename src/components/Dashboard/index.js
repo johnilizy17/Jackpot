bc5be794a1e88fcd060e2fd7c7ef3e76522157b5
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-19 19:46:37 +0100

    Timer

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -166,7 +166,7 @@ export default function DashboardDesktop() {
             const { hash } = await writeContract(config)
 
             setAllowed(amount)
-            const timing = amount === 5 ? 10 : amount === 10 ? 5 : 2.5
+            const timing = amount == 5 ? 10 : amount == 10 ? 5 : 2.5
             setDate(timing)
             toast({ position: "top-right", title: "Approved", description: "Approved successful", status: "success", isClosable: true });
             setMintApproval(true)
