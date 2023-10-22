f0bd3087025a8760007117e12b53b5a254142a07
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-22 06:13:16 +0100

    Disactive

--- src/components/Dashboard/Timer.js
+++ src/components/Dashboard/Timer.js
@@ -97,6 +97,7 @@ export default function TimeCounter({ date, setName, setDate, setLoading2, setDi
         var distance = DownDate - Math.floor(now / 1000);
         // Time calculations for days, hours, minutes and seconds
         if (distance < 15) {
+            toast({ position: "top-right", title: "Stake button disactivate Error", description: "any timer less than 10 seconds all button is activated", status: "error", isClosable: true });
             setDisable(true)
         }
         var hours = Math.floor((distance % (60 * 60 * 24)) / (60 * 60));
