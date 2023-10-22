f63ba6b972e51ca32d4a59af1c9314eb701287f3
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-22 05:20:15 +0100

    Local change

--- src/components/Dashboard/Timer.js
+++ src/components/Dashboard/Timer.js
@@ -72,7 +72,7 @@ export default function TimeCounter({ date, setName, setDate, setLoading2, setDi
                 if (notify) {
 
                 } else if (formatEther(data2[datalength].endTime) * 1000000000000000000 === 0 && exist) {
-                   localStorage.setItem(`${current}${contractAddress}`, "true")
+                   localStorage.setItem(`${current}${contractAddress}1`, "true")
                     setLoading2(true)
                     setWinnerAddress({jackpot:data2[datalength - 1].winner, bomb:data3 && data3.length && data3[data3.length - 1][0]? data3[data3.length - 1][0]:""})
                     if (data2[datalength - 1].winner === address) {
