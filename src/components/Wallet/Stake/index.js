216b6e85893bad5a3fa6059da01f808666e05344
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-23 10:12:17 +0100

    Details

--- src/components/Wallet/Stake/index.js
+++ src/components/Wallet/Stake/index.js
@@ -62,11 +62,8 @@ export default function Stake({ setStaking, setToggle }) {
                       {fetch.map((a, b) => (
                            <tr>
                           <td scope="row" data-label="id">{formatEther(a.id) * 1000000000000000000}</td>
-                           <td data-label="Jackpot Name">${a.name}</td>
-                               <td data-label="Winneer Address">{formatEther(a.winner)}</td>
-                                <td data-label="Date">${formatEther(a.endtime) * 1000000000000000000}</td>
-                               <td data-label="Status">{formatEther(a.winner) === address? "You lost" : "You won"}</td>
-                                   
+                           <td data-label="Jackpot Name">{formatEther(a.name)}</td>
+                               
                           </tr>
                         ))
                         }
