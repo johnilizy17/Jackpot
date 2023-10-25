114e52d944451bc10e658caf3bf1ff7b6747fd1d
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-25 06:01:48 +0100

    disable

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -42,10 +42,10 @@ export default function DashboardDesktop() {
     const { isOpen, onOpen, onClose } = useDisclosure()
 
    useEffect(()=>{
-       if(display){
+       if(disable){
         toast({ position: "top-right", title: "Stake button disactivate Error", description: "any timer less than 10 seconds all button is activated", status: "error", isClosable: true });
         }
-       },[display])
+       },[disable])
     
     async function jackpotInfo() {
         try {
