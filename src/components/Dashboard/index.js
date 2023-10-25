1d6959992b557ad97408bc3121601a20a679d124
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-25 05:44:14 +0100

    New reward

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -322,8 +322,8 @@ export default function DashboardDesktop() {
                         </Box>
                         <Box className="minor-bar">
                             <Box className="labels">
-                                <p>Big Bang</p>
-                                <p>${type ===1 ? bigBangPrice.toFixed(2): '100%'}/${value[1]}</p>
+                                <p>{ type === 1? "Big Bang": "Minor</p>
+                                <p>${type ===1 ? bigBangPrice.toFixed(2)/'$'+value[1]:bigBangPrice.toFixed(2) }</p>
                             </Box>
                             <Box className="progress-bar " style={{overflow:"hidden"}}>
                                 <Box className="bar" style={percentage > 100 ? { width: `100%` } : type != 1 ? { width: `100%` } : { width: `${percentage}%` }}></Box>
