c55a3bad9a872ade52019a22e7a765f6ea230ea3
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-10-29 19:25:16 +0100

    currentTimer()

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -259,6 +259,7 @@ export default function DashboardDesktop() {
             setDownDate(now + minuterSetter);
             }
             setTimeout(()=>{
+             currentTimer()
              jackpotInfo()
              notification()
                setTimeRefresh(!timeRefresh)
