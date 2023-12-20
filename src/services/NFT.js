20cbb2be8c32bf835b48b0a56af775ecb0dbc6c7
Oluwaleke Akanji <83691620+leksite2010@users.noreply.github.com>
2023-12-20 18:43:53 +0100

    contract address upadate

--- src/services/NFT.js
+++ src/services/NFT.js
@@ -1,2 +1,2 @@
-export const contractAddress = "0xB7B7fdffd5B4c0905db7b53fF910Ff27A19Dd92b"
+export const contractAddress = "0x8cAb89947061bce9d2d83f1851D20d1ca7699e6d"
 export const BUSD = "0xaB1a4d4f1D656d2450692D237fdD6C7f9146e814"
