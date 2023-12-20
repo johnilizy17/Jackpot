a5fd2a03637e6a55d3db588ba92cddfd4773873f
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2023-12-20 22:44:36 +0100

    Type

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -399,7 +399,7 @@ export default function DashboardDesktop() {
                 <section className="page">
                     <Box className="body">
                         <Box className="timer">
-                            <TimeCounter setStartTimer={setStartTimer} name={name} startTimer={startTimer} timeRefresh={timeRefresh} date={date} DownDate={DownDate} setDownDate={setDownDate} setLoading2={setLoading2} setDisable={setDisable} setName={setName} setDate={setDate} type={type} />
+                            <TimeCounter setStartTimer={setStartTimer} name={type} startTimer={startTimer} timeRefresh={timeRefresh} date={date} DownDate={DownDate} setDownDate={setDownDate} setLoading2={setLoading2} setDisable={setDisable} setName={setName} setDate={setDate} type={type} />
                         </Box>
 
                         <Display data={jackpotData} bigBang={bigBang} name={name} getCurrentJackpotInfo={getCurrentJackpotInfo} setName={setName} type={type} staker={stake} />
