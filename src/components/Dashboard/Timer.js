07a06ad2aae29dabaedf8b5aad4fb23a5b7ed1e3
John Abraham Tosin <76853683+johnilizy17@users.noreply.github.com>
2024-01-18 08:42:25 +0100

    Type

--- src/components/Dashboard/Timer.js
+++ src/components/Dashboard/Timer.js
@@ -105,7 +105,7 @@ export default function TimeCounter({ setStartTimer, timeRefresh, name, startTim
                     if(notify2){
                         
                     } else if(data3[data3.length - 1].winnerAddress === address){
-                      setWinnerAddress({jackpot:data2[datalength - 1].winner, bomb:data2[datalength - 1].id == 0 && data3 && data3[data3.length - 1].winnerAddress? data3[data3.length - 1].winnerAddress:"", type: })
+                      setWinnerAddress({jackpot:data2[datalength - 1].winner, bomb:data2[datalength - 1].id == 0 && data3 && data3[data3.length - 1].winnerAddress? data3[data3.length - 1].winnerAddress:"", type:exist[numberExist].jType })
                        if(type === 1){
                         setName("bombwinner")
                        }
@@ -208,7 +208,7 @@ let DateObj = new Date();
                                 <p className="blink_me" style={{ color: "rgb(30, 240, 30)", width: 300}} > {winnerAddress.jackpot}</p>
                             </Box>
                         </Box>
-                        <Box display={winnerAddress.bomb === address && winnerAddress.bomb != "" && type === 1 ? "flex" : "none"} w={["100%", "320px"]} h="70px" alignItems="center" p="20px" pt="10px" >
+                        <Box display={winnerAddress.bomb === address && winnerAddress.bomb != "" && winnerAddress.type === 1 ? "flex" : "none"} w={["100%", "320px"]} h="70px" alignItems="center" p="20px" pt="10px" >
                             <Box className="texts second" mt="-10px">
                                 <h4>Bomb Winner Wallet</h4>
                                 <p style={{width:300}}>{winnerAddress.bomb === ""? "Winner in progress": winnerAddress.bomb}</p>
